fd6e10c71aaf5bceb4c42f0120f80f0e1f32ad03
Vivek Solapure <solapurevivek2003@gmail.com>
2023-11-17 22:03:30 +0530

    homepage

--- my-app/src/components/navbar/Home.jsx
+++ my-app/src/components/navbar/Home.jsx
@@ -1,201 +1,184 @@
-import { useState } from 'react';
+import React from 'react'
 import "./home.css"
 import { Link } from "react-router-dom";
 
-export default function Dummyhome() {
-    const addMember=()=>{
 
-    }
-    let a = 1;
-    let b = true;
-    const [index, setIndex] = useState(a)
-    if (index === 9) {
-        setIndex(index - 8)
-    }
-    const [divs = true, setdiv] = useState(b)
-    const btnpressnext = () => {
-        console.log(divs)
-        setdiv(divs)
-        console.log(index)
-        setIndex(index + 1)
-
-    }
-    if (index === 0) {
-        setIndex(index + 8)
-    }
-    const btnpressprev = () => {
-        console.log(index)
-        setIndex(index - 1);
-    }
-
-    const slides = [
-        (<img src="" alt="/" />),
-        (<img className={`slide-img ${divs ? 'slide-imgs' : ''}`} src="./slide-img1.jpg" alt="/" />),
-        (<a href="/"> <img className={`slide-img ${divs ? 'slide-imgs' : ''}`} src="./slide-img2.jpg" alt="/" /> </a>),
-        (<img className={`slide-img ${divs ? 'slide-imgs' : ''}`} src="./profile-pic.png" alt="/" />),
-        (<a href="/"> <img className={`slide-img ${divs ? 'slide-imgs' : ''}`} src="./slide-img3.jpg" alt="/" /> </a>),
-        (<img className={`slide-img ${divs ? 'slide-imgs' : ''}`} src="./slide-img4.jpg" alt="/" />),
-        (<a href="/"> <img className={`slide-img ${divs ? 'slide-imgs' : ''}`} src="./slide-img5.jpg" alt="/" /> </a>),
-        (<img className={`slide-img ${divs ? 'slide-imgs' : ''}`} src="./slide-img6.jpg" alt="/" />),
-        (<a href="/"><img className={`slide-img ${divs ? 'slide-imgs' : ''}`} src="./slide-img7.jpg" alt="/" /></a>),
-        (<img className={`slide-img ${divs ? 'slide-imgs' : ''}`} src="./slide-img8.jpg" alt="/" />),
-    ]
+export default function Test2() {
     return (
         <>
-            <div className="body-container">
-                <img src="./logo.png" alt='/' className="logo" />
 
-                <div className="head-container">
-                    <h2 className='title' >Coding <br />Crafters</h2>
-                    <h4 id="head-para">
-                        Step into Code Club, the ultimate launchpad for future tech buddies
-                        in the realm of CSE engineering. Beyond code, we cultivate
-                        indispensa ble soft skills and communication prowess, s haping
-                        well-rounded Professionals Skills.so get ready toEmbark on a
-                        transformative journey whe re innovation meets holistic growth!
-                        Coding C rafters Club Founded in 2023.
-                    </h4>
-                </div>
-                <div className='line1'><div className='line'></div></div>
-                <div className="slider">
+            <div class="triangle-left">
+                <svg xmlns="http://www.w3.org/2000/svg" width="274" height="696" viewBox="0 0 4 696" fill="none">
+                    <path d="M-249.598 508.253L-25.1932 0.00042571L273.609 695.155L-249.598 508.253Z" fill="url(#paint0_linear_1144_166)" />
+                    <defs>
+                        <linearGradient id="paint0_linear_1144_166" x1="63.4424" y1="488.068" x2="60.9563" y2="6.8411" gradientUnits="userSpaceOnUse">
+                            <stop offset="0.0653348" />
+                            <stop offset="0.589217" stop-color="#1E012F" />
+                            <stop offset="1" stop-color="#5D0093" />
+                        </linearGradient>
+                    </defs>
+                </svg>
+            </div>
+            <div class="triangle-up">
 
-                    <div className="product-carousel">
-                        <button className="pre-btn" onClick={btnpressprev} ><p>&lt;</p></button>
-                        <button className="next-btn" onClick={btnpressnext} ><p>&gt;</p></button>
-                        <div className="product-container">{slides[index]}</div>
+            </div>
+
+            <div className="home_body">
+            <div className="home_grid1">
+                    <div className="home_h1 ">
+                        <h1>Coding <span>Crafters</span></h1>
                     </div>
-                    <div className="slide_info">
-                        <h3 id='slide_info_title'>Title</h3>
-                        <p id='slide_info_para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, ipsum. Nesciunt expedita debitis quam, cum aliquam officia fugit, ea eius praesentium commodi architecto quo quos! Quidem suscipit voluptatem corrupti repellendus. </p> </div>
-                </div>
-                <div className='line1_down'><div className='line_down'></div></div>
-                <div className="member-container">
-                    <Link to="/inbox">
-                     <div className="Member_add">
-                     <p>+</p>
-                     </div>
-                    </Link>
-
-                    <h2 id='member_title' onClick={addMember}>Members</h2>
-
-                    <div className="member-container2">
-
-                        <div className='cardDiv'>
-                            <div className="cardImg">
-                                <img src="./profile-pic.png" id='member_photo' alt="" />
-
-                            </div>
-                            <div className="IncardDiv">
-                                <div className="infoDiv">
-                                    <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
-                                    <h4>Persuing :B-tech</h4>
-                                    <p>selected for</p>
-                                    <h5>2023-2024</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='cardDiv'>
-                            <div className="cardImg">
-                                <img src="./profile-pic.png"id='member_photo' alt="" />
-
-                            </div>
-                            <div className="IncardDiv">
-                                <div className="infoDiv">
-                                    <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
-                                    <h4>Persuing :B-tech</h4>
-                                    <p>selected for</p>
-                                    <h5>2023-2024</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='cardDiv'>
-                            <div className="cardImg">
-                                <img src="./profile-pic.png"id='member_photo' alt="" />
-
-                            </div>
-                            <div className="IncardDiv">
-                                <div className="infoDiv">
-                                    <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
-                                    <h4>Persuing :B-tech</h4>
-                                    <p>selected for</p>
-                                    <h5>2023-2024</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='cardDiv'>
-                            <div className="cardImg">
-                                <img src="./profile-pic.png"id='member_photo' alt="" />
-
-                            </div>
-                            <div className="IncardDiv">
-                                <div className="infoDiv">
-                                    <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
-                                    <h4>Persuing :B-tech</h4>
-                                    <p>selected for</p>
-                                    <h5>2023-2024</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='cardDiv'>
-                            <div className="cardImg">
-                                <img src="./profile-pic.png"id='member_photo' alt="" />
-
-                            </div>
-                            <div className="IncardDiv">
-                                <div className="infoDiv">
-                                    <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
-                                    <h4>Persuing :B-tech</h4>
-                                    <p>selected for</p>
-                                    <h5>2023-2024</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='cardDiv'>
-                            <div className="cardImg">
-                                <img src="./profile-pic.png"id='member_photo' alt="" />
-
-                            </div>
-                            <div className="IncardDiv">
-                                <div className="infoDiv">
-                                    <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
-                                    <h4>Persuing :B-tech</h4>
-                                    <p>selected for</p>
-                                    <h5>2023-2024</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='cardDiv'>
-                            <div className="cardImg">
-                                <img src="./profile-pic.png"id='member_photo' alt="" />
-
-                            </div>
-                            <div className="IncardDiv">
-                                <div className="infoDiv">
-                                    <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
-                                    <h4>Persuing :B-tech</h4>
-                                    <p>selected for</p>
-                                    <h5>2023-2024</h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='cardDiv'>
-                            <div className="cardImg">
-                                <img src="./profile-pic.png"id='member_photo' alt="" />
-
-                            </div>
-                            <div className="IncardDiv">
-                                <div className="infoDiv">
-                                    <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
-                                    <h4>Persuing :B-tech</h4>
-                                    <p>selected for</p>
-                                    <h5>2023-2024</h5>
-                                </div>
-                            </div>
-                        </div>
-
+                    <div className="home_para">
+                        <p>At Code Club, we shape future tech
+                            professionals in CSE engineering while honing essential soft skill
+                            s and communication prowess. Join us on a transformative journey,
+                            where innovation meets holistic growth at the Coding Crafters Club
+                            , established in 2023.
+                        </p>
+                    </div>
+                    <div className="home_JoinUs">
+                        <h1>Join Our <span>Community</span> </h1>
+                        <p>Unlock your coding potentials</p>
+                        <button className="home_JoinUS_btn">Join US</button>
                     </div>
                 </div>
+                <div className="home_logo home_grid2"><img src="./logo.png" alt="" /></div>
+
+                
+                <div className="home_entImgs home_grid3">
+                    <img src="./slide-img2.jpg" alt="" />
+                </div>
+                <div className="home_entInfo home_grid4">
+                    <div className="quote">
+                        <blockquote></blockquote>
+                    </div>
+                    <div className="text">At Code Club, we shape future tech professionals in CSE engineering while honing
+                        essential soft skills and communication prowess. Join us on a transformative journey, where innovation meets holistic
+                        growth at the Coding Crafters Club, established in 2023.<br />Unlock your coding potentials</div>
+                </div>
+
+            </div>
+
+            <div className="member-container2">
+                <h2 id='member_title' >Members</h2>
+
+
+
+                <div className='cardDiv'>
+                    <div className="cardImg">
+                        <img src="./profile-pic.png" id='member_photo' alt="" />
+
+                    </div>
+                    <div className="IncardDiv">
+                        <div className="infoDiv">
+                            <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
+                            <h4>Persuing :B-tech</h4>
+                            <p>selected for</p>
+                            <h5>2023-2024</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className='cardDiv'>
+                    <div className="cardImg">
+                        <img src="./profile-pic.png" id='member_photo' alt="" />
+
+                    </div>
+                    <div className="IncardDiv">
+                        <div className="infoDiv">
+                            <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
+                            <h4>Persuing :B-tech</h4>
+                            <p>selected for</p>
+                            <h5>2023-2024</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className='cardDiv'>
+                    <div className="cardImg">
+                        <img src="./profile-pic.png" id='member_photo' alt="" />
+
+                    </div>
+                    <div className="IncardDiv">
+                        <div className="infoDiv">
+                            <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
+                            <h4>Persuing :B-tech</h4>
+                            <p>selected for</p>
+                            <h5>2023-2024</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className='cardDiv'>
+                    <div className="cardImg">
+                        <img src="./profile-pic.png" id='member_photo' alt="" />
+
+                    </div>
+                    <div className="IncardDiv">
+                        <div className="infoDiv">
+                            <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
+                            <h4>Persuing :B-tech</h4>
+                            <p>selected for</p>
+                            <h5>2023-2024</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className='cardDiv'>
+                    <div className="cardImg">
+                        <img src="./profile-pic.png" id='member_photo' alt="" />
+
+                    </div>
+                    <div className="IncardDiv">
+                        <div className="infoDiv">
+                            <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
+                            <h4>Persuing :B-tech</h4>
+                            <p>selected for</p>
+                            <h5>2023-2024</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className='cardDiv'>
+                    <div className="cardImg">
+                        <img src="./profile-pic.png" id='member_photo' alt="" />
+
+                    </div>
+                    <div className="IncardDiv">
+                        <div className="infoDiv">
+                            <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
+                            <h4>Persuing :B-tech</h4>
+                            <p>selected for</p>
+                            <h5>2023-2024</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className='cardDiv'>
+                    <div className="cardImg">
+                        <img src="./profile-pic.png" id='member_photo' alt="" />
+
+                    </div>
+                    <div className="IncardDiv">
+                        <div className="infoDiv">
+                            <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
+                            <h4>Persuing :B-tech</h4>
+                            <p>selected for</p>
+                            <h5>2023-2024</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className='cardDiv'>
+                    <div className="cardImg">
+                        <img src="./profile-pic.png" id='member_photo' alt="" />
+
+                    </div>
+                    <div className="IncardDiv">
+                        <div className="infoDiv">
+                            <p id='name'>  DEVELOPER <div className='underline1'><div className='underline' ></div></div><h3 id='name'>  Vivek solapure</h3></p>
+                            <h4>Persuing :B-tech</h4>
+                            <p>selected for</p>
+                            <h5>2023-2024</h5>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </>
-    );
+
+    )
 }
